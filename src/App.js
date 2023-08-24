8a601c8a22e0663228d45a039d9b9130eee558ef
unknown <albertkantwi@gmail.com>
2023-08-24 01:46:43 +0100

    Add Calculation.js and Home.js files with code to build full web page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import './index.css';
-import Calculator from './components/Calculator';
+import { Link, Routes, Route } from 'react-router-dom';
+import Calculation from './components/Calculation';
 import Quote from './components/Quote';
+import Home from './components/Home';
 
 const App = () => (
-  <div className="main">
-    <Calculator />
-    <Quote />
-  </div>
+  <>
+    <header className="navbar">
+      <h1 className="page-header">Math Magicians</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/" className="nav-link">Home</Link>
+          </li>
+          <li>|</li>
+          <li>
+            <Link to="/calculator" className="nav-link">Calculator</Link>
+          </li>
+          <li>|</li>
+          <li>
+            <Link to="/quote" className="nav-link">Quote</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <Routes className="main">
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculation />} />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
+  </>
 );
 
 export default App;
---
+++ src/components/Calculation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from './Calculator';
+
+const Calculation = () => (
+  <div className="calculator-body">
+    <div className="description">
+      <h2>Let&apos;s do some math!</h2>
+    </div>
+    <Calculator />
+  </div>
+);
+
+export default Calculation;
